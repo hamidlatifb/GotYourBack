cfe50d33bd8804900c8a06113dbfa0f304e82cb4
Hamid Latif <hamidlatifbaloch0@gmail.com>
2026-08-23 11:45:35 +0500

    feat: implement directory validation and tar compression

--- GotYourBack.sh
+++ GotYourBack.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+read -p "Enter the directory name you want to backup: " file_name
 
+if [ -d "$file_name" ]; then
+	echo "Directory found! Starting the backup............."
+	tar -czvf "${file_name}_backup.tar.gz" "$file_name"
+else
+	echo "Error: Directory '$file_name' does not exist in this path."
+	exit 1
+fi
+
+#cp -r Pictures Pictures_backup.zip
